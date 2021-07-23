669dc371d526b6bb87aecada6cf7417f08129126
alisson728 <85299905+alisson728@users.noreply.github.com>
2021-07-23 13:02:20 -0300

    Add files via upload

---
+++ jsava.js
@@ -0,0 +1,42 @@
+var roupas = [{description: 'Mockup+Arte camisa banda Queen', img: 'https://i.ibb.co/Jv6kGPQ/camisa-queen.png' },
+
+              {description: 'Mockup+Arte camisa anime Attack on Titan', img: 'https://i.ibb.co/C6pd73c/camisa-eren.png'},
+
+              {description: 'Mockup+Arte camisa desenho Pantera cor de rosa', img: 'https://i.ibb.co/xq6xxkc/pantera-car-de-rosa.png'},
+
+              {description: 'Mockup+Arte camisa desenho Rick and Morty', img: 'https://i.ibb.co/zJ4sy0J/rick-and-morty2.png'},
+
+              {description: 'Mockup+Arte camisa anime Berserk', img: ' https://i.ibb.co/fF3s70d/berserk.png'},
+
+              {description: 'Mockup+Arte camisa anime Naruto', img: 'https://i.ibb.co/qd6GmbT/teste.png'},
+
+              {description: 'Mockup+Arte camisa time Flamengo', img: 'https://i.ibb.co/pPNgZhv/flamengo-1.png'},
+
+              {description: 'Mockup+Arte camisa time Flamengo', img: 'https://i.ibb.co/Hhwgcyq/flamengo.png'},
+
+              {description: 'Mockup+Arte camisa time São Paulo', img: 'https://i.ibb.co/PhFkTqk/saopaulo.png'},
+
+              {description: 'Mockup+Arte moletom anime Bleach', img: 'https://i.ibb.co/PZNDGm0/bleach.png'},
+
+              {description: 'Mockup+Arte moletom banda Pink Floyd', img: 'https://i.ibb.co/1Jj4p0d/pinkfloyd.png'},
+
+              {description: 'Mockup+Arte moletom banda Queen', img: 'https://i.ibb.co/0DmygKj/queen.png'},
+
+              {description: 'Mockup+Arte camisa Slesh', img: 'https://i.ibb.co/swsQpvj/slash.png'},
+
+              {description: 'Mockup+Arte camisa Hora de aventra+Apenas um show', img: 'https://i.ibb.co/Y3ByHCM/hora-de-aventiura-e-apenas-um-show.png'},
+
+              {description: 'Mockup+Arte moletom Slesh', img: 'https://i.ibb.co/xGVp53N/moletm-slesh.png'},
+
+              {description: 'Mockup+Arte camisa Jiraya', img: 'https://i.ibb.co/K5SGqZf/jiraya.png'},
+
+              {description: 'Mockup+Arte camisa Panter negra', img: 'https://i.ibb.co/71Rr9ZT/pantera-negra.png'},
+
+              {description: 'Mockup+Arte camisa Boku no hero academia', img: 'https://i.ibb.co/RbpV1FW/boku-no-hero-academia.png'},
+
+              {description: 'Mockup+Arte camisa Boku no hero academia', img: 'https://i.ibb.co/Sw1mmH0/boku-no-hero-academia2.png'},
+        
+
+              {description: 'Mockup+Arte camisa Itachi', img: 'https://i.ibb.co/8zFBMQP/itachi.png'},
+
+              {description: 'Mockup+Arte camisa+Moletom Itachi', img: 'https://i.ibb.co/tckSDtX/itachi2.png'},]
